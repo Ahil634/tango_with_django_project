5ca6b637f0e4a1d00d78bc1309ec8afed29fd40d
“Krishan <2620765G@student.gla.ac.uk>
2024-02-09 18:26:59 +0000

    Completed assessment

--- rango/views.py
+++ rango/views.py
@@ -16,43 +16,41 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 def index(request):
-    
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
+    
+    visitor_cookie_handler(request) 
+    
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-     context_dict = {'boldmessage': 'This tutorial has been put together by Krishan Gobikrishnan.'}
+     context_dict = {'boldmessage': 'This tutorial has been put together by Krishan Gobikrishnan.', 'visits' : request.session.get('visits', 1)}
      if request.session.test_cookie_worked():
         print("TEST COOKIE WORKED!")
+
+        visitor_cookie_handler(request)
+        visits = int(request.session.get('visits', 1))  
+        context_dict['visits'] = visits
         request.session.delete_test_cookie()
+    
      return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
+   
     context_dict = {}
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
+
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
+
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything -
